787d8578d705eea9bbe6c86f0ac782d5f84be4d2
Till Otto <till@t8lab.com>
2023-07-18 08:59:50 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,9 @@ const Home: NextPage = () => {
   return (
     <div className='h-full w-full '>
       <div className="mt-20 absolute inset-0 bg-[url(/status/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
-      <div className="w-full h-40 absolute bg-light-purple dark:purple dark:bg-black">
+      <div className="w-full h-40 absolute bg-gradient-to-r from-purple-300 to-indigo-300 dark:bg-black">
         <div className="sm:ml-0 ml-5 mr-0 mt-3 md:pl-80 md:pr-80 sm:w-full h-full bg-purple-500 dark:bg-black">
-          <div className="h-5 max-w-16">
-            <Image src="/status/GRIP.png" alt="Grip" />
-          </div>
+          Grip
         </div>
       </div>
       <div className='mt-20 w-full absolute overflow-scroll	'>
